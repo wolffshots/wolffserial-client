8b43dfc9e37ae2b8d9f75e6e8a590712dc6d88ce
jadon <16850875+wolffshots@users.noreply.github.com>
2021-07-16 16:45:49 +0200

    removed register_input

--- src/control.cpp
+++ src/control.cpp
@@ -5,23 +5,13 @@
 volatile uint32_t last_run_time_0 = 0;
 volatile uint32_t last_run_time_1 = 0;
 
-void IRAM_ATTR register_input()
-{
-    Serial.println("DEBUG: register start");
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("DEBUG: register end");
-}
-
 void IRAM_ATTR button_0_handler()
 {
     if (millis() - DEBOUNCE_TIME >= last_run_time_0)
     {
         last_run_time_0 = millis();
         Serial.println("BUTTON:0");
-        // digitalWrite(LED_BUILTIN, HIGH);
-        register_input();
+        digitalWrite(LED_BUILTIN, HIGH);
     }
 }
 
@@ -31,7 +21,6 @@ void IRAM_ATTR button_1_handler()
     {
         last_run_time_1 = millis();
         Serial.println("BUTTON:1");
-        // digitalWrite(LED_BUILTIN, LOW);
-        register_input();
+        digitalWrite(LED_BUILTIN, LOW);
     }
 }
